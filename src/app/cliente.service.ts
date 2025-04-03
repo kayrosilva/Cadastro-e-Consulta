383573af87d71ce1087a055ab7a3e7badf195412
Kayro Silva <kayro.g.silva21@gmail.com>
2025-04-03 19:05:54 -0300

    42Deletando

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -17,7 +17,41 @@ export class ClienteService {
     localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
   }
 
-  obterStorage() : Cliente[] {
+  atualizar(cliente: Cliente){
+    const storage = this.obterStorage();
+    storage.forEach(c => {
+      if(c.id === cliente.id){
+        Object.assign(c, cliente);
+      }
+    })
+    localStorage.setItem(ClienteService.REPO_CLIENTES,JSON.stringify(storage));
+  }
+
+  deletar(cliente: Cliente){
+    const storage = this.obterStorage();
+
+    const novalista = storage.filter(c => c.id !== cliente.id)
+
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(novalista));
+  }
+
+  pesquisarClientes(nomeBusca: string) : Cliente[] {
+
+    const clientes = this.obterStorage();
+
+    if(!nomeBusca){
+      return clientes;
+    }
+
+    return clientes.filter(cliente => cliente.nome?.indexOf(nomeBusca) !== -1)
+  }
+
+  buscarClientePorId(id: string) : Cliente | undefined {
+    const clientes = this.obterStorage();
+    return clientes.find(cliente => cliente.id === id)
+  }
+
+  private obterStorage() : Cliente[] {
     const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
     if(repositorioClientes){
       const clientes: Cliente[] = JSON.parse(repositorioClientes);

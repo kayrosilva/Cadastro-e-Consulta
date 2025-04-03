ae4cd140a276c1928a8e6d1efb87140d0b2a1be9
Kayro Silva <kayro.g.silva21@gmail.com>
2025-04-02 21:09:11 -0300

    32logicacadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule } from '@angular/forms'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { Cliente } from './cliente'
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FlexLayoutModule, 
+    MatCardModule, 
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
 
+  cliente: Cliente = Cliente.newCliente();
+
+  constructor(private service: ClienteService){
+
+  }
+
+  salvar(){
+    this.service.salvar(this.cliente);    
+  }
 }
